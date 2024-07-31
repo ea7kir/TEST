476aa5290f62d1eb62874433234b48b9115bafb1
Michael Naylor <mikenaylorspain@icloud.com>
2024-07-31 19:09:41 +0200

    updated

---
+++ etc/extras.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# https://dustinpfister.github.io/2020/03/27/linux-raspbian-lite-xserver-xorg/
+
+sudo apt-get install xserver-xorg
+
+sudo apt-get install xinit
+
+sudo apt-get install x11-xserver-utils
+
+sudo apt-get install blackbox
+
+sudo apt-get install xterm
+
+# startx
+
+# sudo apt-get install lightdm
+
+
+
+
+
+
+
